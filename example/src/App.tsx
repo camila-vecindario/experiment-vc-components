b32183e5f22e83ea38598c3686a9ed36db9242d2
Jesus Martinez <jesus.martinez@vecindario.com>
2022-01-24 12:57:41 -0500

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@vecindario/vecindario-suite-components'
+import '@vecindario/vecindario-suite-components/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
